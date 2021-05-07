64a132f361e10c93b63ebcf80e7439b10f24df93
river0801 <83344186+river0801@users.noreply.github.com>
2021-05-07 22:34:48 +0900

    login register

--- mainDesign/app/src/main/java/com/example/stock/LoginActivity.java
+++ mainDesign/app/src/main/java/com/example/stock/LoginActivity.java
@@ -7,7 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -28,20 +35,55 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        Button loginButton = (Button) findViewById(R.id.loginButton);
+        final EditText idText = (EditText)findViewById(R.id.idText);
+        final EditText passwordText = (EditText)findViewById(R.id.passwordText);
+        final Button loginButton = (Button)findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //로그인에 성공할 경우
-                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                dialog = builder.setMessage("로그인에 성공했습니다.")
-                        .setPositiveButton("확인", null)
-                        .create();
-                        dialog.show();
-                Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
-                LoginActivity.this.startActivity(mainIntent);
+            public void onClick(View v) {
+                String userID = idText.getText().toString();
+                String userPassword = passwordText.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                         try {
+                             JSONObject jsonResponse = new JSONObject(response);
+                             boolean success = jsonResponse.getBoolean("success");
+                             if (success) {
+                                 AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                 dialog = builder.setMessage("로그인에 성공했습니다.")
+                                         .setPositiveButton("확인", null)
+                                         .create();
+                                 dialog.show();
+                                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                 LoginActivity.this.startActivity(intent);
+                                 finish();
+                             } else {
+                                 AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                 dialog = builder.setMessage("가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.")
+                                         .setNegativeButton("다시 시도", null)
+                                         .create();
+                                 dialog.show();
+                             }
+                         } catch (Exception e) {
+                             e.printStackTrace();
+                         }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
             }
         });
+    }
+
+    protected void onStop() {
+        super.onStop();
+        if(dialog != null) {
+            dialog.dismiss();
+            dialog = null;
+        }
     }
 }
